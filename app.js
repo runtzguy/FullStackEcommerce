810dfb861c7c101004a76a513eee885602203f88
Victor <Victor@Victors-MBP.hitronhub.home>
2019-11-17 16:12:26 -0800

    removed express.static('./public')

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 // app.use(expressSession({secret: "cat", saveUninitialized: false, resave: false}));
 app.use(morgan('short'));
-app.use(express.static('./public')); 
+// app.use(express.static('./public')); 
 
 //Load View Engine
 // app.set('views', path.join(__dirname, 'views'))
