3f2a4f316ee13cf9100bf145abb6f9aa9a91ecc9
Victor <Victor@Victors-MBP.hitronhub.home>
2019-11-21 01:20:10 -0800

    added options to accessControlRequestMethod and accessControlAllowHeaders with Authorization

--- api/routes/userTransHist.js
+++ api/routes/userTransHist.js
@@ -82,7 +82,8 @@ app.post('/', upload.none(), (req,res) => {
     const db = mysql.createConnection(db_config);
     console.log("Database connected for transaction");
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Request-Method', 'POST');
+    res.setHeader('Access-Control-Request-Method', 'POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Authorization');
     res.setHeader('Access-Control-Allow-Credentials', false);
 
     jwt.verify(token, 'verysecretkey', (err, coded)=>{
